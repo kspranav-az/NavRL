85782f1e4983146b40fe384e661fffb2c022fb94
Pranav Dutthan K S <k.s.pranav.2004@gmail.com>
2025-09-02 21:33:00 +0530

    minor fixes

--- isaac-training/training/scripts/env.py
+++ isaac-training/training/scripts/env.py
@@ -847,13 +847,13 @@ class NavigationEnv(IsaacEnv):
         above_bound = self.root_state[..., 2] > 15.0
         
         # Collision penalty
-        self.reward[collision.unsqueeze(-1).expand_as(self.reward)] -= 10.0
+        self.reward[collision.unsqueeze(-1)] -= 10.0
         
         # Goal achievement bonus
-        self.reward[reach_goal.unsqueeze(-1).expand_as(self.reward)] += 50.0
+        self.reward[reach_goal.unsqueeze(-1)] += 50.0
         
         # Multiple collision penalty (discourage repeated crashes)
-        repeated_collision_mask = (self.collision_count > 3).expand_as(self.reward)
+        repeated_collision_mask = self.collision_count > 3
         self.reward[repeated_collision_mask] -= 5.0
 
         # Terminal conditions
@@ -864,7 +864,7 @@ class NavigationEnv(IsaacEnv):
         self.prev_drone_vel_w = vel_w.clone()
 
         # Update statistics
-        self.stats["return"] += self.reward
+        self.stats["return"] += self.reward.squeeze(-1)
         self.stats["episode_len"][:] = self.progress_buf.unsqueeze(1)
         self.stats["reach_goal"] = reach_goal.float().unsqueeze(-1)
         self.stats["collision"] = collision.float().unsqueeze(-1)
